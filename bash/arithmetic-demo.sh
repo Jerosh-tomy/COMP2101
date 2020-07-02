0f8a5f5d518f51c9c0e63b8880f887832a262ee9
Jerosh-tomy <jeroshtomy33@gmail.com>
2020-07-01 20:41:20 -0700

    Update arithmetic-demo.sh

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 # this script demonstrates doing arithmetic
 
 # Task 1: Remove the assignments of numbers to the first and second number variables. Use one or more read commands to get 3 numbers from the user.
@@ -7,7 +6,7 @@
 #    the sum of the 3 numbers with a label
 #    the product of the 3 numbers with a label
 
-# Ask user for 3 numbers
+# Ask the user for 3 numbers
 echo "Enter first number"
 read firstnum
 
